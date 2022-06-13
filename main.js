28074a757ce2b1cef76ae8bc0305c76f0e4a99fa
Joseph Sisay <sisayj7@gmail.com>
2022-06-13 22:25:09 +0100

    Arrays,map and if condition added

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 alert("Welcome to Ninja quiz.");
 
-const question = "What is superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+const quiz = [
+  ["What is Superman's real name?", "Clark Kent"],
+  ["What is Wonder Woman's real name?", "Diana Prince"],
+  ["What is Batman's real name", "Bruce Wayne"],
+];
+
+let score = 0;
+
+for (const [question, answer] of quiz) {
+  const response = prompt(question);
+  if (response === answer) {
+    alert("Correct!");
+    score++;
+  } else {
+    alert(`Wrong! The correct answer is ${answer}.`);
+  }
+}
+
+alert(`Game over! You scored ${score} point${score !== 1 ? "s" : ""}`);
