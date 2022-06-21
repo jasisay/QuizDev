ea69533b6e8c27647856c0384c0d671e67a1e9ca
Joseph Sisay <sisayj7@gmail.com>
2022-06-21 19:58:18 +0100

    Added namespacing and objects to pronect

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 alert("Welcome to Ninja quiz.");
 
 const quiz = [
-  ["What is Superman's real name?", "Clark Kent"],
-  ["What is Wonder Woman's real name?", "Dianne Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"],
+  { name: "Superman", realName: "Clark Kent" },
+  { name: "Wonder Woman", realName: "Diane Prince" },
+  { name: "Batman", realName: "Bruce Wayne" },
 ];
 
-function start(quiz) {
-  let score = 0;
+// namespacing functions
+const game = {
+  start(quiz) {
+    this.questions = [...quiz];
+    this.score = 0;
 
-  // main game loop
-  for (const [question, answer] of quiz) {
-    const response = ask(question);
-    check(response, answer);
-  }
+    // main loop
+    for (const question of this.questions) {
+      this.question = question;
+      this.ask();
+    }
+    //end of main loop
+    this.gameOver();
+  },
 
-  //end of main loop.
+  ask() {
+    const question = `What is ${this.question.name}'s real name?`;
+    const response = prompt(question);
+    this.check(response);
+  },
 
-  gameOver();
-
-  // function declarations
-  function ask(question) {
-    return prompt(question);
-  }
-
-  function check(response, answer) {
+  check(response) {
+    const answer = this.question.realName;
     if (response === answer) {
       alert("Correct!");
-      score++;
+      this.score++;
     } else {
-      alert(`Wrong! The correct answer is ${answer}.`);
+      alert(`Wrong! The correct answer was ${answer}.`);
     }
-  }
+  },
+};
 
-  function gameOver() {
-    alert(`Game over! You scored ${score} point${score !== 1 ? "s" : ""}`);
-  }
-}
-
-start(quiz);
+game.start(quiz);
