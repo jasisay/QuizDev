e4086eb84abacd463ecd46ecf403c84b7a68704e
Joseph Sisay <sisayj7@gmail.com>
2022-06-27 22:06:07 +0100

    Dynamically added questions to the DOM

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ const quiz = [
   { name: "Batman", realName: "Bruce Wayne" },
 ];
 
+//viewObject
+const view = {
+  score: document.querySelector("#score strong"),
+  question: document.getElementById("question"),
+  result: document.getElementById("result"),
+  info: document.getElementById("info"),
+  render(target, contents, attributes) {
+    for (const key in attributes) {
+      target.setAttribute(key, attributes[key]);
+    }
+    target.innerHTML = contents;
+  },
+};
+
 // namespacing functions
 const game = {
   start(quiz) {
@@ -23,6 +37,7 @@ const game = {
 
   ask() {
     const question = `What is ${this.question.name}'s real name?`;
+    view.render(this.question, question);
     const response = prompt(question);
     this.check(response);
   },
@@ -30,11 +45,23 @@ const game = {
   check(response) {
     const answer = this.question.realName;
     if (response === answer) {
+      view.render(view.result, "Correct!", { "class": "correct" });
       alert("Correct!");
       this.score++;
+      view.render(view.score, this.score);
     } else {
+      view.render(view.result, `Wrong! The correct answer was ${answer}`, {
+        "class": "wrong",
+      });
       alert(`Wrong! The correct answer was ${answer}.`);
     }
+  },
+
+  gameOver() {
+    view.render(
+      view.info,
+      `Game over. You scored ${this.score} point${score !== 1 ? "s" : ""}`
+    );
   },
 };
 
